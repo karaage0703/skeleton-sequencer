b71bc42ef915594e2b86902210dd29913d612d71
karaage <coldchicken@gmail.com>
2021-07-30 01:09:23 +0900

    Update

--- skeleton_sequencer_mediapipe.py
+++ skeleton_sequencer_mediapipe.py
@@ -6,6 +6,13 @@ import argparse
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
+import time
+
+# time param
+start_time = 0.0
+dot_line = 0
+
+HUMAN_COLOR = (0, 255, 0)
 
 
 def get_args():
@@ -34,7 +41,59 @@ def get_args():
     return args
 
 
+def skeleton_sequencer(src):
+    global start_time
+    global dot_line
+
+    # parameters
+    speed = 0.5
+    d_circle = 30
+
+    image_h, image_w = src.shape[:2]
+
+    h_max = int(image_h / d_circle)
+    w_max = int(image_w / d_circle)
+
+    # create blank image
+    npimg_target = np.zeros((image_h, image_w, 3), np.uint8)
+    dot_color = [[0 for i in range(h_max)] for j in range(w_max)]
+
+    # make dot information from ndarray
+    for y in range(0, h_max):
+        for x in range(0, w_max):
+            dot_color[x][y] = src[y * d_circle][x * d_circle]
+
+    # move dot
+    while time.time() - start_time > speed:
+        start_time += speed
+        dot_line += 1
+        if dot_line > w_max - 1:
+            dot_line = 0
+
+    # draw dot
+    for y in range(0, h_max):
+        for x in range(0, w_max):
+            center = (int(x * d_circle + d_circle * 0.5), int(y * d_circle + d_circle * 0.5))
+            if x == dot_line:
+                print(dot_color[dot_line][y])
+                if dot_color[dot_line][y].tolist() == list(HUMAN_COLOR):
+                    cv.circle(npimg_target, center, int(d_circle / 2), [
+                        255 - (int)(dot_color[x][y][0]), 255 - (int)(dot_color[x][y][1]), 255 - (int)(dot_color[x][y][2])],
+                        thickness=-1, lineType=8, shift=0)
+                else:
+                    cv.circle(npimg_target, center, int(d_circle / 2), [
+                        255, 255, 255], thickness=-1, lineType=8, shift=0)
+            else:
+                cv.circle(npimg_target, center, int(d_circle / 2), [
+                    (int)(dot_color[x][y][0]), (int)(dot_color[x][y][1]), (int)(dot_color[x][y][2])],
+                    thickness=-1, lineType=8, shift=0)
+
+    return npimg_target
+
+
 def main():
+    global start_time
+
     args = get_args()
 
     cap_device = args.device
@@ -58,6 +117,7 @@ def main():
         min_tracking_confidence=min_tracking_confidence,
     )
 
+    start_time = time.time()
     while True:
         # camera capture
         ret, image = cap.read()
@@ -81,7 +141,14 @@ def main():
         if key == 27:  # ESC
             break
 
-        cv.imshow('MediaPipe Pose Demo', debug_image)
+        image_ss = skeleton_sequencer(debug_image)
+
+        key = cv.waitKey(1)
+        if key == 27:  # ESC
+            break
+
+        # cv.imshow('MediaPipe Pose Demo', debug_image)
+        cv.imshow('MediaPipe Pose Demo', image_ss)
 
     cap.release()
     cv.destroyAllWindows()
@@ -112,8 +179,10 @@ def draw_landmarks(
     visibility_th=0.5,
 ):
     image_width, image_height = image.shape[1], image.shape[0]
-    face_size = int(image_width * 0.25)
-    body_line_size = int(image_width * 0.01)
+    face_size = int(image_width * 0.3)
+    face_offset = int(image_width * 0.05)
+    body_line_size = int(image_width * 0.1)
+    hand_line_size = int(image_width * 0.1)
 
     landmark_point = []
 
@@ -126,122 +195,130 @@ def draw_landmarks(
         if landmark.visibility < visibility_th:
             continue
 
-        if index == 0:  # nose
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), face_size)
+        if index == 0:  # Head
+            cv.circle(image, (landmark_x, landmark_y - face_offset), 5, HUMAN_COLOR, face_size)
 
     if len(landmark_point) > 0:
         # shoulder
         if landmark_point[11][0] > visibility_th and landmark_point[12][
                 0] > visibility_th:
             cv.line(image, landmark_point[11][1], landmark_point[12][1],
-                    (0, 255, 0), body_line_size)
+                    HUMAN_COLOR, body_line_size)
 
         # right arm
         if landmark_point[11][0] > visibility_th and landmark_point[13][
                 0] > visibility_th:
             cv.line(image, landmark_point[11][1], landmark_point[13][1],
-                    (0, 255, 0), body_line_size)
+                    HUMAN_COLOR, body_line_size)
         if landmark_point[13][0] > visibility_th and landmark_point[15][
                 0] > visibility_th:
             cv.line(image, landmark_point[13][1], landmark_point[15][1],
-                    (0, 255, 0), body_line_size)
+                    HUMAN_COLOR, body_line_size)
 
         # left arm
         if landmark_point[12][0] > visibility_th and landmark_point[14][
                 0] > visibility_th:
             cv.line(image, landmark_point[12][1], landmark_point[14][1],
-                    (0, 255, 0), body_line_size)
+                    HUMAN_COLOR, body_line_size)
         if landmark_point[14][0] > visibility_th and landmark_point[16][
                 0] > visibility_th:
             cv.line(image, landmark_point[14][1], landmark_point[16][1],
-                    (0, 255, 0), body_line_size)
+                    HUMAN_COLOR, body_line_size)
 
         # right hand
-        # if landmark_point[15][0] > visibility_th and landmark_point[17][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[15][1], landmark_point[17][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[17][0] > visibility_th and landmark_point[19][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[17][1], landmark_point[19][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[19][0] > visibility_th and landmark_point[21][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[19][1], landmark_point[21][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[21][0] > visibility_th and landmark_point[15][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[21][1], landmark_point[15][1],
-        #             (0, 255, 0), 2)
+        if landmark_point[15][0] > visibility_th and landmark_point[17][
+                0] > visibility_th:
+            cv.line(image, landmark_point[15][1], landmark_point[17][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[17][0] > visibility_th and landmark_point[19][
+                0] > visibility_th:
+            cv.line(image, landmark_point[17][1], landmark_point[19][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[19][0] > visibility_th and landmark_point[21][
+                0] > visibility_th:
+            cv.line(image, landmark_point[19][1], landmark_point[21][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[21][0] > visibility_th and landmark_point[15][
+                0] > visibility_th:
+            cv.line(image, landmark_point[21][1], landmark_point[15][1],
+                    HUMAN_COLOR, hand_line_size)
 
         # left hand
-        # if landmark_point[16][0] > visibility_th and landmark_point[18][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[16][1], landmark_point[18][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[18][0] > visibility_th and landmark_point[20][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[18][1], landmark_point[20][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[20][0] > visibility_th and landmark_point[22][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[20][1], landmark_point[22][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[22][0] > visibility_th and landmark_point[16][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[22][1], landmark_point[16][1],
-        #             (0, 255, 0), 2)
+        if landmark_point[16][0] > visibility_th and landmark_point[18][
+                0] > visibility_th:
+            cv.line(image, landmark_point[16][1], landmark_point[18][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[18][0] > visibility_th and landmark_point[20][
+                0] > visibility_th:
+            cv.line(image, landmark_point[18][1], landmark_point[20][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[20][0] > visibility_th and landmark_point[22][
+                0] > visibility_th:
+            cv.line(image, landmark_point[20][1], landmark_point[22][1],
+                    HUMAN_COLOR, hand_line_size)
+        if landmark_point[22][0] > visibility_th and landmark_point[16][
+                0] > visibility_th:
+            cv.line(image, landmark_point[22][1], landmark_point[16][1],
+                    HUMAN_COLOR, hand_line_size)
 
-        # body
-        # if landmark_point[11][0] > visibility_th and landmark_point[23][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[11][1], landmark_point[23][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[12][0] > visibility_th and landmark_point[24][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[12][1], landmark_point[24][1],
-        #             (0, 255, 0), 2)
-        # if landmark_point[23][0] > visibility_th and landmark_point[24][
-        #         0] > visibility_th:
-        #     cv.line(image, landmark_point[23][1], landmark_point[24][1],
-        #             (0, 255, 0), 2)
+        # body shape
+        if landmark_point[11][0] > visibility_th and landmark_point[23][
+                0] > visibility_th:
+            cv.line(image, landmark_point[11][1], landmark_point[23][1],
+                    HUMAN_COLOR, body_line_size)
+        if landmark_point[12][0] > visibility_th and landmark_point[24][
+                0] > visibility_th:
+            cv.line(image, landmark_point[12][1], landmark_point[24][1],
+                    HUMAN_COLOR, body_line_size)
+        if landmark_point[23][0] > visibility_th and landmark_point[24][
+                0] > visibility_th:
+            cv.line(image, landmark_point[23][1], landmark_point[24][1],
+                    HUMAN_COLOR, body_line_size)
+
+        # fill body
+        if landmark_point[11][0] > visibility_th and landmark_point[12][
+                0] > visibility_th and landmark_point[23][
+                0] > visibility_th and landmark_point[24][
+                0] > visibility_th:
+            cv.rectangle(image, landmark_point[11][1], landmark_point[24][1],
+                         HUMAN_COLOR, -1)
 
         if len(landmark_point) > 25:
             # right foot
             if landmark_point[23][0] > visibility_th and landmark_point[25][
                     0] > visibility_th:
                 cv.line(image, landmark_point[23][1], landmark_point[25][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[25][0] > visibility_th and landmark_point[27][
                     0] > visibility_th:
                 cv.line(image, landmark_point[25][1], landmark_point[27][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[27][0] > visibility_th and landmark_point[29][
                     0] > visibility_th:
                 cv.line(image, landmark_point[27][1], landmark_point[29][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[29][0] > visibility_th and landmark_point[31][
                     0] > visibility_th:
                 cv.line(image, landmark_point[29][1], landmark_point[31][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
 
             # left foot
             if landmark_point[24][0] > visibility_th and landmark_point[26][
                     0] > visibility_th:
                 cv.line(image, landmark_point[24][1], landmark_point[26][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[26][0] > visibility_th and landmark_point[28][
                     0] > visibility_th:
                 cv.line(image, landmark_point[26][1], landmark_point[28][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[28][0] > visibility_th and landmark_point[30][
                     0] > visibility_th:
                 cv.line(image, landmark_point[28][1], landmark_point[30][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
             if landmark_point[30][0] > visibility_th and landmark_point[32][
                     0] > visibility_th:
                 cv.line(image, landmark_point[30][1], landmark_point[32][1],
-                        (0, 255, 0), 2)
+                        HUMAN_COLOR, body_line_size)
     return image
 
 
